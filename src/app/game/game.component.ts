85264722f293b0ea9c8fea16f8f90a43d32ecc58
Tomas <tomas.egf@gmail.com>
2020-07-14 09:48:57 -0400

    Update game.component.ts

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+import { DynamicScriptLoader } from '../dynamic-script-loader.service';
+//import { start } from '../../assets/js/canvas.js';
+
 declare var canvasjs:any;
 
 @Component({
@@ -11,5 +15,13 @@ export class GameComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //this.loadScripts();
+    //start();
   }
+  //private loadScripts() {
+    // You can load multiple scripts by just providing the key as argument into load method of the service
+    //DynamicScriptLoader.loadScript('chartjs').then(data => {
+      // Script Loaded Successfully
+   // }).catch(error => console.log(error));
+  //}
 }
