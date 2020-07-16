87bb86eeae8c79c82f6da7a9dd56d8c71ef974a6
Tomas Gaete <tegaete@uc.cl>
2020-07-16 11:33:44 -0400

    fixed on server functions and some errors

--- src/assets/js/canvas.js
+++ src/assets/js/canvas.js
@@ -1,9 +1,11 @@
 
 
+
+
 color = "black";
 cookie = window.localStorage.getItem('cookie');
 var addr = 'https://rtsketchserver-kbefbqorma-ue.a.run.app';
-addr = 'http://192.168.1.4:3030';
+//addr = 'http://192.168.1.4:3030';
 window.addEventListener("load", ()=> {
     console.log('local storage cookie: ' + window.localStorage.getItem('cookie'))         
     console.log('local storage username: ' + window.localStorage.getItem('username'))         
@@ -30,7 +32,10 @@ function load_events_register(){
 }
 
 function load_events_home(){
-    try{document.querySelector("#btn_login").addEventListener("mousedown", function(){ login();});   }
+    try{
+        document.querySelector("#btn_login").addEventListener("mousedown", function(){ login();});   
+        
+    }
     catch{}
 }
 
@@ -233,7 +238,7 @@ l1 = 0;
                     window.localStorage.setItem('cookie', data.cookie);
                     console.log('logged in')
 
-                    console.log('local storage: ' + window.localStorage.getItem('cookie'))      
+                    console.log('local storage: cookie ' + window.localStorage.getItem('cookie'))      
                     update_visuals();
                     window.location.replace('/user')
                 }else{
@@ -250,14 +255,16 @@ l1 = 0;
         var request = new XMLHttpRequest()
         request.open('POST', addr + '/api/v1/logout?session_cookie='+c, true)
         request.onload = function () {
+            window.localStorage.setItem('cookie', null);
+            window.localStorage.setItem('username', null);
             var data = JSON.parse(this.response)
             if (data.data == 'ok'){   
-                window.localStorage.setItem('cookie', '');
-                window.localStorage.setItem('username', '');
+                window.localStorage.setItem('cookie', null);
+                window.localStorage.setItem('username', null);
                 update_visuals();
                 window.location.replace('/home')
             }else{
-                console.log('error de logout')
+                console.log('error de logout ' + data)
             }
             console.log(data)    
                 
@@ -280,6 +287,7 @@ l1 = 0;
         passconf = document.querySelector("#inputPasswordConfirmation").value;
         if (pass == passconf){
             send_registry(un, lang, email, pass);
+            alert("registro creado");
         }
         else{
             console.log('register error: pass != passconf');
@@ -322,7 +330,7 @@ l1 = 0;
     }
 
     function is_logged_in(){
-        if(window.localStorage.getItem('cookie') == ""){
+        if(window.localStorage.getItem('cookie') == "null"){
             console.log('no cookie 2');
             return false;
         }else{
@@ -331,9 +339,7 @@ l1 = 0;
     }
 
     //creates a tag to show the user is logged in
-    function show_username(){
-        var un =   window.localStorage.getItem('username');
-        if(un == ""){return;}
+    function show_username(un){
         var tag = document.createElement("p");
         tag.setAttribute("id", "menu_username_text");
 
@@ -344,8 +350,6 @@ l1 = 0;
     }
 
     function show_logout(){
-        var un = window.localStorage.getItem('username');
-        if(un == ""){return;}
         var tag = document.createElement("p");
         tag.setAttribute("id", "menu_logout_text");
 
@@ -372,6 +376,40 @@ l1 = 0;
         element.appendChild(tag);
     }
 
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+    function replace_login(){
+        try{
+        var elmnt = document.getElementById("login-card");
+        removeAllChildNodes(elmnt);
+        var elmnt = document.getElementById("guest-card");
+        removeAllChildNodes(elmnt);
+
+        var elmnt = document.getElementById("login-card-f");
+        removeAllChildNodes(elmnt);
+
+        id="login-card-f"
+        
+        //var tag0 = document.createElement("div");
+        //tag0.setAttribute("class", "col-lg-6 col-md-6 align-middle");
+
+        var tag = document.createElement("button");
+        var text = document.createTextNode("let's play!");
+        tag.setAttribute("class", "btn btn-lg btn-primary btn-block");
+        tag.setAttribute("id", "btn_lets_play");
+        tag.style.display = 'block';
+        tag.style.padding = '10px';
+        tag.appendChild(text);
+        var element = document.getElementById('login-card');
+        element.appendChild(tag);
+        document.querySelector("#btn_lets_play").addEventListener("mousedown", function(){  window.location.replace('/game');});  
+    }
+    catch{}
+    }
+
     function update_visuals(){
         try{var elmnt = document.getElementById("menu_username_text");
         elmnt.remove();
@@ -379,6 +417,16 @@ l1 = 0;
         elmnt.remove();
         }
         catch{}
-        show_logout();
-        show_username();
+
+        var un = window.localStorage.getItem('username');
+        if(un == "null"){
+            console.log('no used dependant buttons');
+            return;
+        }else{
+        
+            show_logout();
+            show_username(un);
+            replace_login();
+        }
+        
     }
